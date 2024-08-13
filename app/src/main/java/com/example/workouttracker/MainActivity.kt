79b064e6432a69a08256227050a3ddfd5d65b0eb
AntoOfo <148264049+AntoOfo@users.noreply.github.com>
2024-08-13 23:04:58 +0100

    Menu layout and new activity created for steps counter. button intent leads to new activity

--- app/src/main/java/com/example/workouttracker/MainActivity.kt
+++ app/src/main/java/com/example/workouttracker/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.workouttracker
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +13,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val stepsButton = findViewById<Button>(R.id.stepsBtn)
+        val heartButton = findViewById<Button>(R.id.heartBtn)
+        val caloriesButton = findViewById<Button>(R.id.caloriesBtn)
+
+        stepsButton.setOnClickListener{
+            val intent = Intent(this, StepsActivity::class.java)
+            startActivity(intent)
+        }
         }
     }
-}
