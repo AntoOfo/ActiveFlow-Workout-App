2c25cc97e426d4dc30df80d33d90a0dfa4a7795f
AntoOfo <148264049+AntoOfo@users.noreply.github.com>
2024-08-14 22:12:46 +0100

    functionality for step counter working and rough xml

--- app/src/main/java/com/example/workouttracker/StepsActivity.kt
+++ app/src/main/java/com/example/workouttracker/StepsActivity.kt
@@ -1,20 +1,86 @@
 package com.example.workouttracker
 
+import android.content.Context
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlin.math.sqrt
 
-class StepsActivity : AppCompatActivity() {
+class StepsActivity : AppCompatActivity(), SensorEventListener {        // brings in sensor listener
+
+    private lateinit var sensorManager: SensorManager  //initialised later (lateinit)
+    private var accelerometer: Sensor? = null
+    private var lastAcceleration: Float = 0f        // holds previous acceleration value
+    private var currentAcceleration: Float = 0f     // holds current acceleration value
+    private var acceleration: Float = 0f
+    private val shakeThreshold = 11f    // shake detection
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_steps)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val stepTitle = findViewById<TextView>(R.id.stepsTitle)
+        val stepsCounter = findViewById<TextView>(R.id.stepCounter)
+
+        sensorManager =
+            getSystemService(Context.SENSOR_SERVICE) as SensorManager   // initialises sensormanager
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+
+        // initalising acceleration values
+        lastAcceleration = SensorManager.GRAVITY_EARTH
+        currentAcceleration = SensorManager.GRAVITY_EARTH
+        acceleration = 0.00f
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        // allows sensor to start listening for updates
+        accelerometer?.also {
+            sensorManager.registerListener(this, it, SensorManager.SENSOR_DELAY_UI)
         }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        // when activity is paused, unregisters listener
+        sensorManager.unregisterListener(this)
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {     // when new sensor data is available
+        if (event?.sensor?.type == Sensor.TYPE_ACCELEROMETER) { // if event is from the accelerometer
+            val x = event.values[0]
+            val y = event.values[1]
+            val z = event.values[2]
+
+            val lastAcceleration = currentAcceleration
+            currentAcceleration = sqrt(x * x + y * y + z * z)
+            val delta = currentAcceleration - lastAcceleration      // change in acceleration
+            acceleration = acceleration * 0.9f + delta
+
+            if (acceleration > shakeThreshold) {
+                onShakeDetected()
+            }
+        }
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+        // null
+    }
+
+    private fun onShakeDetected() {
+
+        val stepsCounter = findViewById<TextView>(R.id.stepCounter)
+        var currentSteps = stepsCounter.text.toString().toInt()
+        currentSteps += 1
+        stepsCounter.text = currentSteps.toString()
     }
 }
