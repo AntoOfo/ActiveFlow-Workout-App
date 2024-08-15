027338bd3db556b6832917b32c581b7a6611422e
AntoOfo <148264049+AntoOfo@users.noreply.github.com>
2024-08-15 21:38:59 +0100

    dropdown works, now need to make logic

--- app/src/main/java/com/example/workouttracker/CaloriesActivity.kt
+++ app/src/main/java/com/example/workouttracker/CaloriesActivity.kt
@@ -1,14 +1,25 @@
 package com.example.workouttracker
 
+import android.icu.text.Transliterator.Position
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.RadioButton
+import android.widget.Spinner
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.textfield.TextInputEditText
 
-class CaloriesActivity : AppCompatActivity() {
+class CaloriesActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
+
+    val levels: Array<String> = arrayOf("Sedentary", "Lightly Active",
+        "Moderately Active", "Very Active", "Super Active")
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -19,5 +30,25 @@ class CaloriesActivity : AppCompatActivity() {
         val weightInput = findViewById<TextInputEditText>(R.id.weightInput)
         val heightInput = findViewById<TextInputEditText>(R.id.heightInput)
         val ageInput = findViewById<TextInputEditText>(R.id.ageInput)
+
+
+        val dropdown = findViewById<Spinner>(R.id.activityDropdown)
+        dropdown.onItemSelectedListener = this  // tells which item in list is clicked
+
+        // array adapter adds list items to dropdown
+        val add: ArrayAdapter<*> = ArrayAdapter<Any?>(this,
+                                        android.R.layout.simple_spinner_item, levels)
+
+        dropdown.adapter = add      // binds data to dropdown
+
         }
+
+    override fun onItemSelected(parent: AdapterView<*>?, view: View, position: Int, id: Long) {
+
+        Toast.makeText(applicationContext, levels[position], Toast.LENGTH_LONG).show()
     }
+
+    override fun onNothingSelected(p0: AdapterView<*>?) {
+        // Null
+    }
+}
